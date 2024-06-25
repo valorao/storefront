297fc211779254ce5abf7b153597b3d82fad1dfc
Isaque Weskerson <isaquewrodrigues@icloud.com>
2024-06-25 18:33:00 -0300

    Update endpoints.

--- app/api/storefront/route.ts
+++ app/api/storefront/route.ts
@@ -4,7 +4,7 @@ import { authOptions } from "@/lib/auth-options";
 
 export const GET = async () => {
     const session = await getServerSession(authOptions);
-    const response = await fetch("https://rso.rtrampox.cloud/v1/riot/oauth/player/storefront", {
+    const response = await fetch("https://rso.rtrampox.cloud/v1/riot/player/storefront", {
         method: "PUT",
         headers: {
             'Authorization': `${(session?.user as { valorantToken?: string })?.valorantToken}`
