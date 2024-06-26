e9394a2a74dc0eecff28aa9e614435077f37b763
Isaque Weskerson <isaquewrodrigues@icloud.com>
2024-06-26 02:21:54 -0300

    Update logout flow.

--- components/ProfileDropdownMenu.tsx
+++ components/ProfileDropdownMenu.tsx
@@ -12,8 +12,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
 function logoutUser() {
-    signOut()
-    redirect('https://oauth.rtrampox.cloud/api/logout?redirect_uri=https://valorao.cloud')
+    signOut({ redirect: false, callbackUrl: "https://oauth.rtrampox.cloud/api/logout?redirect_uri=https://valorao.cloud" })
 }
 
 export default function ProfileDropdownMenu() {
