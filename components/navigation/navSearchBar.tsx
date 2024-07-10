8162f40588d66fd22d088f8b09d24234afde3a83
Isaque Weskerson <isaquewrodrigues@icloud.com>
2024-07-10 18:13:43 -0300

    Fix search bar size in some devices.

--- components/navigation/navSearchBar.tsx
+++ components/navigation/navSearchBar.tsx
@@ -62,7 +62,7 @@ export default function SearchBar({ open, setOpen }: { open: boolean, setOpen: (
     return (
         <div className="flex items-center flex-col justify-center">
             <Dialog open={open} onOpenChange={setOpen}>
-                <DialogContent className={errors.search ? "border-red-700 p-1" : ""}>
+                <DialogContent className={`w-96 md:w-full ${errors.search ? "border-red-700 p-1" : ""}`}>
                     <div className="flex flex-row items-center">
                         <div className="flex-1 flex-row items-center mr-2">
                             <form
@@ -140,7 +140,7 @@ const DialogContent = React.forwardRef<
         <DialogPrimitive.Content
             ref={ref}
             className={cn(
-                "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+                "fixed left-[50%] top-[50%] z-50 grid w-full max-w-2xl translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-lg",
                 className
             )}
             {...props}
